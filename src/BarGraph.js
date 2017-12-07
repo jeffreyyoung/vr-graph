531588a66949ec4cad87404ea095ad6231d65000
Andrea Yang <ayang@adobe.com>
2017-12-07 17:11:33 -0500

    Add legend

--- src/BarGraph.js
+++ src/BarGraph.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Bar from './Bar';
 import _ from 'lodash';
 import colorPalette from './Colors'
+import {Entity} from 'aframe-react';
+import Text from './Text';
 class BarGraph extends React.Component {
   static get defaultProps() {
     return {
@@ -46,8 +48,8 @@ class BarGraph extends React.Component {
       .flatten()
       .filter(j => j)
       .map(r => {
-        console.log('max', max);
-        console.log('R?', r.value, normalize(r.value, min, max));
+        // console.log('max', max);
+        // console.log('R?', r.value, normalize(r.value, min, max));
         
         return {
           ...r,
@@ -64,10 +66,50 @@ class BarGraph extends React.Component {
     const distanceAwayFromGraph = -20;
     const xStart = 0 - ( (this.props.data.length * (barWidth+spacingBetweenBars)) / 2 );
     const yStart = 0;
-    return data.map(({value, color}, i) => {
-        const height = value;
-        return (<Bar key={i} color={color} height={height + yStart} y={height/2 + yStart} width={barWidth} z={distanceAwayFromGraph} x={xStart + (i*(barWidth+spacingBetweenBars))}/>)
-      })
+
+    const defaultProps = {
+      color: '#000',
+      value: 'Default text',
+      width: 0,
+      lineHeight: 38,
+      letterSpacing: 0,
+      align: 'left',
+      opacity: 1,
+      zOffset: 0,
+    }
+
+    let keyLength = Object.keys(this.state.colors).length;
+    const legendContents = Object.keys(this.state.colors).map((val, i) => {
+      return (
+        <Text
+          key={i}
+          value={"---- "+val}
+          color={this.state.colors[val]}
+          width={4}
+          zOffset={-10}
+          x={i*2.5 * (2/keyLength)}
+          y={-0.2}
+        />
+      )
+    });
+
+    const stuffToRender = data.map(({value, color}, i) => {
+      const height = value;
+      return (<Bar key={i} color={color} height={height + yStart} y={height/2 + yStart} key={i} width={barWidth} z={distanceAwayFromGraph} x={xStart + (i*(barWidth+spacingBetweenBars))}/>)
+    });
+
+    stuffToRender.push((
+      <Entity
+        geometry={{primitive: 'box', width: Object.keys(this.state.colors).length + 3, height: 1}}
+        material={{color: '#FFFFFF', roughness: 0.5}}
+        scale={{x: 2, y: 2, z: 0}}
+        position={{x: 0, y: 0, z: -5}}
+      >
+        <Text value="LEGEND" color="#000" width={6} align="center" position="0 0.2 0" letterSpacing={5} />
+        {legendContents}
+      </Entity>));
+
+    return stuffToRender;
   }
 }
 
